91de79e59c1851d40f868d13f572538fe59e0872
Noel Martínez Pomares <nmp50@alu.ua.es>
2022-09-29 14:48:15 +0200

    básico

---
+++ Jugador.py
@@ -0,0 +1,26 @@
+import random
+from Posicion import *
+
+#Clase que define a los jugadores de la partida
+class Jugador:
+    def __init__ (self,alias):
+        self.posicion = Posicion(random.randint(1,20),random.randint(1,20))
+        self.alias = alias  #pasado como parametro?
+        self.nivel = 1
+        self.EF = random.randint(-10,10)
+        self.EC = random.randint(-10,10)
+        self.muerto = False
+
+    #Decrementa en una cierta cantidad el nivel del jugador
+    def decrementarNivel(self, cantidad):
+        self.nivel -= cantidad
+
+        #El nivel mínimo es 0
+        if self.nivel < 0:
+            self.nivel = 0
+    
+    def matar(self):
+        self.muerto = True
+    
+    def vivoOmuerto(self):
+        return self.muerto
