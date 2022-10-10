41bc24e6ee7216c0bc394510356af6401093a93e
Noel Martínez Pomares <nmp50@alu.ua.es>
2022-10-10 10:00:53 +0200

    Creadas las clases Engine y Registry, y modificado Jugador

---
+++ AA_Registry.py
@@ -0,0 +1,66 @@
+# recibe por parametro "puerto de escucha"
+# La aplicación permanecerá a la espera hasta recibir una solicitud de registro de un jugador. 
+# Los datos que puede guardar de un jugador son: alias, password, nivel, EF, EC
+
+import socket 
+import threading
+
+HEADER = 64
+PORT = 5050
+FORMAT = 'utf-8'
+FIN = "FIN"
+
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+MAX_CONEXIONES = 5     #nº conexiones que puede conectar a la vez
+
+def handle_client(conn, addr):
+    #print(f"[NUEVA CONEXION] {addr} connected.")
+
+    connected = True
+
+    while connected:
+        msg_length = conn.recv(HEADER).decode(FORMAT)
+
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = conn.recv(msg_length).decode(FORMAT)
+
+            if msg == FIN:
+                connected = False
+            #print(f" He recibido del cliente [{addr}] el mensaje: {msg}")
+            conn.send(f"Soy el servidor y he recibido tu mensaje: {msg} ".encode(FORMAT))
+    #print("ADIOS. TE ESPERO EN OTRA OCASION")
+    conn.close()
+    
+        
+
+def start():
+    server.listen()
+    #print(f"[LISTENING] Servidor a la escucha en {SERVER}")
+    CONEX_ACTIVAS = threading.active_count()-1
+    #print(CONEX_ACTIVAS)
+
+    while True:
+        conn, addr = server.accept()
+        CONEX_ACTIVAS = threading.active_count()
+
+        if (CONEX_ACTIVAS <= MAX_CONEXIONES): 
+            thread = threading.Thread(target=handle_client, args=(conn, addr))
+            thread.start()
+            #print(f"[CONEXIONES ACTIVAS] {CONEX_ACTIVAS}")
+            #print("CONEXIONES RESTANTES PARA CERRAR EL SERVICIO", MAX_CONEXIONES-CONEX_ACTIVAS)
+        else:
+            #print("OOppsss... DEMASIADAS CONEXIONES. ESPERANDO A QUE ALGUIEN SE VAYA")
+            conn.send("Demasiadas conexiones. Tendrás que esperar a que alguien se vaya".encode(FORMAT))
+            conn.close()
+            CONEX_ACTUALES = threading.active_count()-1
+        
+######################### MAIN ##########################
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDR)
+
+#print("[STARTING] Servidor inicializándose...")
+
+start()
--- Jugador.py
+++ Jugador.py
@@ -1,11 +1,20 @@
 import random
+import socket
+from ssl import _PasswordType
+import sys
+from telnetlib import EC
 from Posicion import *
+
+HEADER = 64
+PORT = 5050
+FORMAT = 'utf-8'
+FIN = "FIN"
 
 #Clase que define a los jugadores de la partida
 class Jugador:
-    def __init__ (self,alias):
+    def __init__ (self):
         self.posicion = Posicion(random.randint(1,20),random.randint(1,20))
-        self.alias = alias  #pasado como parametro?
+        self.alias = ""
         self.nivel = 1
         self.EF = random.randint(-10,10)
         self.EC = random.randint(-10,10)
@@ -18,12 +27,64 @@ class Jugador:
         #El nivel mínimo es 0
         if self.nivel < 0:
             self.nivel = 0
+
+    def asignarAlias(self,alias):
+        self.alias = alias
     
+    def obtenerNivel(self):
+        return self.nivel
+    
+    def obtenerEC(self):
+        return self.EC
+
+    def obtenerEF(self):
+        return self.EF
+
     def matar(self):
         self.muerto = True
     
     def vivoOmuerto(self):
         return self.muerto
+
+
+
+    def send(msg):
+        message = msg.encode(FORMAT)
+        msg_length = len(message)
+        send_length = str(msg_length).encode(FORMAT)
+        send_length += b' ' * (HEADER - len(send_length))
+        client.send(send_length)
+        client.send(message)
+    
+
+    def main():
+        if  (len(sys.argv) == 5):
+            SERVER = sys.argv[1]
+            PORT = int(sys.argv[2])
+            ADDR = (SERVER, PORT)
+            
+            client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            client.connect(ADDR)
+            #print (f"Establecida conexión en [{ADDR}]")
+
+            ALIAS=sys.argv[3]
+            PASSWORD=sys.argv[4]
+            NIVEL=self.obtenerNivel()
+            EC=sys.argv[6]
+            EF=sys.argv[7]
+
+            while msg != FIN :
+                #print("Envio al servidor: ", msg)
+                send(msg)
+                #print("Recibo del Servidor: ", client.recv(2048).decode(FORMAT))
+                msg=input()
+
+            #print ("SE ACABO LO QUE SE DABA")
+            #print("Envio al servidor: ", FIN)
+            send(FIN)
+            client.close()
+        else:
+            #print ("Oops!. Parece que algo falló. Necesito estos argumentos: <ServerIP> <Puerto> <ALIAS> <PASSWORD>")
 
 
 #para pillar las teclas del jugador usamos en python, msvrct.getch()decode(FORMAT)
